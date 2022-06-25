3ad4bfd0f4df9aa09f3d691b2675e3d6e0a07fdb
Ridgewell <ridgewell@ava.solutions>
2022-06-25 14:10:17 +0200

    add auth incomplete

---
+++ Dinner.Contracts/Authentication/AuthenticationResponse.cs
@@ -0,0 +1,12 @@
+namespace Dinner.Contracts.Authentication;
+
+public record AuthenticationResponse
+(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string Token,
+);
+
+
